3111e302b5ff9e01fce6f2d256fa9536bd17757b
holuwamayowah <mmodukoya@gmail.com>
2022-12-09 01:53:50 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char alp[52] =
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0;m i < 52; i++)
+{
+
+putchar(alp[i]);
+}
+putchar("\n");
+
+return (0);
+
+}

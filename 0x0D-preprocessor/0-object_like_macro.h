5ec2cfffad19ee9ee7a2fedbc18e850a3bbf40a6
holuwamayowah <mmodukoya@gmail.com>
2023-01-16 09:21:08 -0800

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,6 @@
-#ifndef OBJECT_MACRO_H
-#define OBJECT_MACRO_H
+#ifndef SIZE_H
+#define SIZE_H
 
-/**
- * file - 0-object_like_macro.h
- * Description: A header that defines a macro SIZE abbreviated
- * as a token - 1024
- */
-
-#define SIZE (1024)
+#define SIZE 1024
 
 #endif
